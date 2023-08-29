384c13aae7887cf9d7604d799dbfa413d2399a79
MarcosH911 <marcosh.craft@gmail.com>
2023-08-29 11:53:03 +0200

    added route link to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Home() {
   return (
     <div className="flex h-full items-center justify-center">
-      <Link href="/">
+      <Link href="/play">
         <button className="animate-up-down rounded-lg bg-teal-500 px-10 py-4 text-5xl font-bold uppercase tracking-wider text-black shadow-xl duration-150 hover:scale-105 hover:shadow-2xl hover:animate-paused active:scale-100 active:shadow-xl">
           Play
         </button>
