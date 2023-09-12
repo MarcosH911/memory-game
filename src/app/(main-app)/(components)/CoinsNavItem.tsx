684b117f9f9da31a9f17d490df6eead06022d8d5
MarcosH911 <marcosh.craft@gmail.com>
2023-09-12 15:33:48 +0200

    Revert "Refactored navbar points and diamonds into route handler"
    This reverts commit a5eb00a46cb7c7d8a97577d89c62d41cefadcea4.

--- src/app/(main-app)/(components)/CoinsNavItem.tsx
+++ src/app/(main-app)/(components)/CoinsNavItem.tsx
@@ -1,30 +1,12 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { BiSolidCoinStack } from "react-icons/bi";
 
-function CoinsNavItem() {
-  const [coins, setCoins] = useState(0);
-
-  useEffect(() => {
-    const fetchPoints = async () => {
-      const pointsDataResponse = await fetch("/api/points/user-points", {
-        method: "get",
-      });
-
-      const { data: userPointsData } = await pointsDataResponse.json();
-      setCoins(userPointsData.total_coins);
-    };
-
-    fetchPoints();
-  }, []);
-
+const CoinsNavItem = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-w-[8rem] items-center justify-between rounded-full border-2 border-yellow-600 px-2 py-1">
-      <span className="ml-1 font-bold text-yellow-600">{coins}</span>
+      <span className="ml-1 font-bold text-yellow-600">{children}</span>
       <BiSolidCoinStack className="text-lg text-yellow-600" />
     </div>
   );
-}
+};
 
 export default CoinsNavItem;
--- src/app/(main-app)/(components)/DiamondsNavItem.tsx
+++ src/app/(main-app)/(components)/DiamondsNavItem.tsx
@@ -1,31 +1,12 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { IoDiamond } from "react-icons/io5";
 
-function DiamondsNavItem() {
-  const [diamonds, setDiamonds] = useState(0);
-
-  useEffect(() => {
-    const fetchPoints = async () => {
-      const pointsDataResponse = await fetch("/api/points/user-points", {
-        method: "get",
-      });
-
-      const { data: userPointsData } = await pointsDataResponse.json();
-
-      setDiamonds(userPointsData.total_diamonds);
-    };
-
-    fetchPoints();
-  }, []);
-
+const DiamondsNavItem = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-w-[8rem] items-center justify-between rounded-full border-2 border-purple-600 px-2 py-1">
-      <span className="ml-1 font-bold text-purple-600">{diamonds}</span>
+      <span className="ml-1 font-bold text-purple-600">{children}</span>
       <IoDiamond className="text-lg text-purple-600" />
     </div>
   );
-}
+};
 
 export default DiamondsNavItem;
