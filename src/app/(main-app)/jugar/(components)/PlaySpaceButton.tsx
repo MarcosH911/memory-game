36cce46aa1605258b2eb7fdebad2c38128129516
MarcosH911 <marcosh.craft@gmail.com>
2023-09-18 19:51:24 +0200

    fixed spacebar responsive bug

--- src/app/(main-app)/jugar/(components)/PlaySpaceButton.tsx
+++ src/app/(main-app)/jugar/(components)/PlaySpaceButton.tsx
@@ -32,7 +32,7 @@ function PlaySpaceButton({ isSpacePressed, setIsSpacePressed }: Props) {
       onClick={() => setIsSpacePressed(true)}
       ref={spaceButtonRef}
       className={twMerge(
-        "relative xl:h-24 xl:max-w-xl md:max-w-lg h-20 2xl:h-28 w-[min(24rem,95vw)] 2xl:max-w-3xl -translate-y-2 rounded-lg border-4 border-slate-600 bg-slate-100 shadow-xl shadow-black/60 transition duration-300 focus:outline-none my-2 sm:max-w-md",
+        "relative xl:h-24 xl:max-w-xl md:max-w-lg h-20 2xl:h-28 w-full max-w-[min(24rem,95vw)] 2xl:max-w-3xl -translate-y-2 rounded-lg border-4 border-slate-600 bg-slate-100 shadow-xl shadow-black/60 transition duration-300 focus:outline-none my-2 sm:max-w-md",
         isSpacePressed &&
           "translate-y-0 bg-slate-200 shadow-sm shadow-black/30",
       )}
