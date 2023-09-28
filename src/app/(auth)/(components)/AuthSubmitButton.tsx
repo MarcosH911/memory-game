ef09624709c6bea7541d5c2b2244b9b0c9824fb9
unknown <marcosh.craft@gmail.com>
2023-09-28 08:59:04 +0200

    added small loading mouse detail to auth button

--- src/app/(auth)/(components)/AuthSubmitButton.tsx
+++ src/app/(auth)/(components)/AuthSubmitButton.tsx
@@ -12,7 +12,7 @@ function AuthSubmitButton({ disabled, text }: Props) {
     <button
       type="submit"
       disabled={disabled}
-      className="relative mx-auto mt-4 w-auto rounded-lg bg-teal-800 px-10 py-2 text-lg font-semibold text-white shadow-lg transition hover:bg-teal-900 active:shadow-none disabled:bg-slate-400"
+      className="relative mx-auto mt-4 w-auto rounded-lg bg-teal-800 px-10 py-2 text-lg font-semibold text-white shadow-lg transition hover:bg-teal-900 active:shadow-none disabled:bg-slate-400 disabled:cursor-wait"
     >
       <Spinner visible={disabled} size="2xl" />
       <span className={twMerge("visible", disabled && "invisible")}>
