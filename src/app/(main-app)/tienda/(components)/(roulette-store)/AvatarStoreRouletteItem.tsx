73cde66bc418e144f40aa9c734b7df1a8ff6e464
MarcosH911 <marcosh.craft@gmail.com>
2023-10-08 09:31:07 +0200

    added sizes to tutorial images

--- src/app/(main-app)/tienda/(components)/(roulette-store)/AvatarStoreRouletteItem.tsx
+++ src/app/(main-app)/tienda/(components)/(roulette-store)/AvatarStoreRouletteItem.tsx
@@ -9,7 +9,7 @@ function AvatarStoreRouletteItem({ data, index }: Props) {
   if (!data) return null;
 
   return (
-    <div className="flex aspect-square w-[calc((100vw-1rem)/3)] flex-shrink-0 items-center justify-center bg-gradient-to-b from-pink-500 via-red-500 to-yellow-400 p-2 xs:p-3 md:w-[calc((100vw-1rem)/5)] xl:w-[min(calc((100vw-1rem)/5),16rem)] xl:p-4">
+    <div className="flex aspect-square w-[calc((100vw-1rem)/3)] flex-shrink-0 items-center justify-center bg-gradient-to-b from-pink-500 via-red-500 to-yellow-400 p-2 md:w-[calc((100vw-1rem)/5)] xl:w-[min(calc((100vw-1rem)/5),16rem)] xl:p-4 xs:p-3">
       <div className="relative h-full w-full">
         <Image
           priority={index <= 5}
@@ -17,7 +17,7 @@ function AvatarStoreRouletteItem({ data, index }: Props) {
           src={data}
           alt="Avatar image"
           fill
-          sizes="calc((100vw-1rem)/3, (min-width: 768px) h-[calc((100vw-1rem)/5)], (min-width: 1280px) 80rem"
+          sizes="calc((100vw-1rem)/3, (min-width: 768px) calc((100vw-1rem)/5), (min-width: 1280px) 80rem"
           quality={index > 5 && index < 90 ? 1 : 75}
           className="rounded-full border-2 border-teal-950 bg-teal-950 xs:border-4"
         />
